5300f206a75efc0bead2cf5258d3b359397d6696
Andrii Furmanets <afurm@Andriis-MacBook-Air.local>
2024-02-17 09:47:46 +0200

    Fixed pages cutted off

--- apps/daos/widget/daos/Layouts/App.jsx
+++ apps/daos/widget/daos/Layouts/App.jsx
@@ -14,7 +14,6 @@ const Theme = styled.div`
   position: fixed;
   inset: 73px 0px 0px;
   width: 100%;
-  height: 100%;
   min-height: 100vh;
   display: flex;
   flex-direction: column;
@@ -82,8 +81,8 @@ const Theme = styled.div`
 
 const Container = styled.div`
   width: 100%;
-  height: 100%;
   min-height: 100vh;
+  overflow-y: scroll;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -140,7 +139,6 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  height: 100%;
   width: 75%;
   margin: 3rem 0;
 `;
