57cd1a098a4b680151afe36cf9c17688d36dcedd
Andrii Furmanets <afurm@Andriis-MacBook-Air.local>
2024-02-21 12:02:06 +0200

    Added contract call for likes

--- apps/daos/widget/daos/Pages/Proposals/List.jsx
+++ apps/daos/widget/daos/Pages/Proposals/List.jsx
@@ -24,27 +24,31 @@ if (!items) return <Widget src="flashui.near/widget/Loading" />;
 
 return (
   <Container>
-    <div className="mb-4">
-      <a
-        style={{ background: "#A4C2FD" }}
-        className="btn-primary"
-        href={`//*__@replace:widgetPath__*/.App?page=create_proposal&daoId=${daoId}`}
-      >
-        <span style={{ fontSize: "24px", width: "20%", margin: "auto" }}>
-          CREATE POST
-        </span>
-      </a>
-    </div>
-    <div className="d-flex flex-column gap-4">
-      {items &&
-        items
-          .filter((i) => i.post_type === type)
-          .map((item, index) => (
-            <Widget
-              src="/*__@replace:widgetPath__*/.Components.Item"
-              props={{ item, index, type }}
-            />
-          ))}
-    </div>
+    {daoId && (
+      <>
+        <div className="mb-4">
+          <a
+            style={{ background: "#A4C2FD" }}
+            className="btn-primary"
+            href={`//*__@replace:widgetPath__*/.App?page=create_proposal&daoId=${daoId}`}
+          >
+            <span style={{ fontSize: "24px", width: "20%", margin: "auto" }}>
+              CREATE POST
+            </span>
+          </a>
+        </div>
+        <div className="d-flex flex-column gap-4">
+          {items &&
+            items
+              .filter((i) => i.post_type === type)
+              .map((item, index) => (
+                <Widget
+                  src="/*__@replace:widgetPath__*/.Components.Item"
+                  props={{ item, index, type, id: item.id }}
+                />
+              ))}
+        </div>
+      </>
+    )}
   </Container>
 );
